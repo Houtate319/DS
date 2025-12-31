de4eeaa64af53d8e749a4917d87c03f211cefe56
Houtate319 <houtate.said5737.encg@uhp.ac.ma>
2025-12-31 09:28:12 +0100

    Update 24010355_houtate_saïd.py

--- 24010355-HOUTATE_SAÏD/24010355_houtate_saïd.py
+++ 24010355-HOUTATE_SAÏD/24010355_houtate_saïd.py
@@ -14,12 +14,18 @@ Original file is located at
 # ------------------------------------------------------------------------------
 # 1. IMPORTATION DES BIBLIOTHÈQUES
 # ------------------------------------------------------------------------------
+
+pip install numpy pandas matplotlib seaborn
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Modules Scikit-Learn spécifiques
+
+pip install scikit-learn
+
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
